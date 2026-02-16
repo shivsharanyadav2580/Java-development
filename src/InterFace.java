c0e3afeb393a217a2f23ab62c0dda42e7e33df28
Shivsharan yadav <shivsharanyadav9516@gmail.com>
2026-02-16 10:43:24 +0530

    Interface practice

---
+++ src/InterFace.java
@@ -0,0 +1,51 @@
+import java.sql.SQLOutput;
+
+interface Lalbahadur{
+    void shop(int income , int years);
+    void education(String businessName , int years);
+
+
+}
+class Bhuvneshvar implements Lalbahadur{
+    public void shop(int income , int years){
+        System.out.println("Bhuvneshvar build businees of revenue :  " + income + "in "  + years );
+
+
+    }
+    public void education(String businessName , int years){
+        System.out.println("Bhuvneshvar build   " + businessName + " business in" + years );
+
+
+    }
+}
+
+class Shivsharan implements Lalbahadur{
+    public void shop(int income , int years){
+        System.out.println("Shivsharan earn income " + income + "in " + years);
+
+    }
+
+    public void education(String developer , int years){
+        System.out.println("Shivsharan develop carrer" + developer + "in " + years);
+
+    }
+}
+
+
+
+
+public class InterFace {
+    static void main(String[] args) {
+      Bhuvneshvar bh =   new Bhuvneshvar();
+      bh.shop(50000,1);
+      bh.education("Lic" , 1);
+
+      Shivsharan sh = new Shivsharan();
+      sh.shop(800000, 2);
+      sh.education("Java developer",1);
+
+
+    }
+
+
+}
