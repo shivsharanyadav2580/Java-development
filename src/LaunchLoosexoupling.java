9f93e5d7b2fb6f7a0dd9b7b4d595d24790faa319
Shivsharan yadav <shivsharanyadav9516@gmail.com>
2026-02-18 12:08:16 +0530

    Lambda ex 2

---
+++ src/LaunchLoosexoupling.java
@@ -0,0 +1,18 @@
+class A {
+    void disp(){
+        System.out.println("Telusko .co ");
+    }
+}
+class B extends A {
+    void show(){
+//        A a = new A();
+//        a.disp();
+        disp();
+    }
+}
+
+
+public class LaunchLoosexoupling {
+
+
+}
