da26fa2a625bb15a1fd9a94907c2f617636f06df
Shivsharan yadav <shivsharanyadav9516@gmail.com>
2026-03-02 17:13:45 +0530

    First commit

---
+++ ExceptionHAndling/src/main/java/SyncAsyncException.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SyncAsyncException {
+    static void main(String[] args) {
+      Scanner sc =  new Scanner(System.in);
+      String name = sc.next();
+      String grdae = sc.next();
+
+
+
+    }
+}
